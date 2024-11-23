94a30dd149c87ef526a76fdc11cf16ddc2e0e57a
LisaDaVinchie <lisadavinchie@gmail.com>
2024-11-23 17:45:45 +0100

    Updated script to create dataset

--- src/data_preprocessing/create_dataset.py
+++ src/data_preprocessing/create_dataset.py
@@ -3,16 +3,20 @@ from pathlib import Path
 from torchvision import transforms
 import random
 import torch as th
-from ImageDataset import CustomImageDataset
+import sys
+
+
 
 # Declare the path to the train images folder
-train_images_path = Path("../data/tiny-imagenet-200/train/")
+train_images_path = Path("../../data/tiny-imagenet-200/train/")
 
 # Declare the path to the train images inner folder
 image_subpath = "images"
 
 # Declare the destination folder
-destination_folder = Path("../data/datasets/")
+destination_folder = Path("../../data/datasets/")
+
+
 
 images_per_class: int = 10
 
@@ -27,28 +31,27 @@ train_images_folder = [f for f in train_images_path.iterdir() if f.is_dir()]
 
 # Create a dictionary to store the images, with the key being the image name and the value being the image tensor
 # taking 10 random images from each class
-images_dict = {}
+images_list = []
 for folder in train_images_folder:
     images = list((folder / image_subpath).glob("*.JPEG"))
     selected_images = random.sample(images, images_per_class)
     
-    folder_name = folder.name
-    
-    images_dict[folder_name] = []
+    label = folder.name
     
     for image in selected_images:
         img = Image.open(image).convert("RGB")
+        
+        tensor_image = transforms.ToTensor()(img)
 
-        images_dict[folder_name].append(transforms.ToTensor()(img))
+        images_list.append((tensor_image, label))
 
 
-# Create the dataset
-
-dataset = CustomImageDataset(images_dict)
+# # Create the dataset
+# dataset = CustomImageDataset(images_list)
 
 # Check if the destination folder exists
 destination_folder.mkdir(parents=True, exist_ok=True)
 
 # Save the dataset
 destination_path = destination_folder / f"dataset_{images_per_class}_images_per_class.pth"
-th.save(dataset, destination_path)
+th.save(images_list, destination_path)
