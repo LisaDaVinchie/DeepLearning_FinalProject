9f581fac18eb25b8837fab9f8fd38ee8b2d0cf6a
LisaDaVinchie <lisadavinchie@gmail.com>
2024-11-25 10:42:41 +0100

    Created autoencoder class

---
+++ src/models/autoencoder.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+class InpaintingAutoencoder(nn.Module):
+    def __init__(self, in_channels=3, middle_channels= [64, 128, 256], kernel_size = [3, 3, 3], stride = [2, 2, 2], padding = [1, 1, 1], output_padding = [1, 1, 1]):
+        super(InpaintingAutoencoder, self).__init__()
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, middle_channels[0], kernel_size=kernel_size[0], stride=stride[0], padding=padding[0]),  # 64x64 -> 32x32
+            nn.ReLU(),
+            nn.Conv2d(middle_channels[0], middle_channels[1], kernel_size=kernel_size[1], stride=stride[1], padding= padding[1]),  # 32x32 -> 16x16
+            nn.ReLU(),
+            nn.Conv2d(middle_channels[1], middle_channels[2], kernel_size=kernel_size[2], stride=stride[2], padding=padding[2]),  # 16x16 -> 8x8
+            nn.ReLU()
+        )
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(middle_channels[2], middle_channels[1], kernel_size=kernel_size[2], stride=stride[2], padding=padding[0], output_padding=1),  # 8x8 -> 16x16
+            nn.ReLU(),
+            nn.ConvTranspose2d(middle_channels[1], middle_channels[0], kernel_size=kernel_size[1], stride=stride[1], padding=padding[1], output_padding=1),  # 16x16 -> 32x32
+            nn.ReLU(),
+            nn.ConvTranspose2d(middle_channels[0], in_channels, kernel_size=kernel_size[0], stride=stride[0], padding=padding[2], output_padding=1),  # 32x32 -> 64x64
+            nn.Sigmoid()  # Output between 0 and 1
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
