30b33a6954d540c200317741d153ce2b15ed470f
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-02 21:14:18 +0100

    Created train sbatch script

---
+++ src/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=train_model
+#SBATCH --output=logs.%j.out
+#SBATCH --error=logs.%j.err
+#SBATCH --time=0-00:30
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+set -e
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    source ../../DL_env/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+fi
+
+make train || { echo "Training failed"; exit 1; }
