eeb3e9ba1cf1003c140dc8108bfd886e38fea31c
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-02 11:54:00 +0100

    Updated train code to get variables from json files

--- src/models/train.py
+++ src/models/train.py
@@ -7,40 +7,46 @@ from autoencoder import Autoencoder_conv, Autoencoder_unet
 from losses import batch_MSE_loss
 import torch.optim as optim
 from tqdm.auto import trange
-import sys
 import time
+import json
+import argparse
 print("Imported all libraries")
 
-if len(sys.argv) < 3:
-    print("Usage: python train.py <train_dataset_path> <test_dataset_path>")
-    exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_train", type=int, required=True, help="Number of training samples")
+parser.add_argument("--n_test", type=int, required=True, help="Number of testing samples")
+parser.add_argument("--n_classes", type=int, required=True, help="Number of classes in the dataset")
+parser.add_argument("--mask_percentage", type=float, required=True, help="Percentage of the image to be masked")
+parser.add_argument("--config_path", type=Path, required=True, help="Path to the config file")
 
-train_dataset_path = Path(sys.argv[1])
-test_dataset_path = Path(sys.argv[2])
+args = parser.parse_args()
 
+n_train = args.n_train
+n_test = args.n_test
+n_classes = args.n_classes
+mask_percentage = args.mask_percentage
+config_path = args.config_path
 
+with open(config_path, "r") as f:
+    config = json.load(f)
 
-N_TRAIN = 10000
-N_TEST = 1000
-N_CLASSES = 50
-MASK_PERCENTAGE = 5
+train_dataset_path = Path(config["train_path"])
+test_dataset_path = Path(config["test_path"])
+weights_folder = Path(config["weights_folder"])
+losses_folder = Path(config["losses_folder"])
 
-identifier = f"{N_TRAIN}_{N_TEST}_{N_CLASSES}_{MASK_PERCENTAGE}"
-
-model_folder = Path("../data/weights/autoencoder/")
-weights_folder = Path("../data/weights/autoencoder/")
-figure_folder = Path("../figs/losses/autoencoder/")
+identifier = f"{n_train}_{n_test}_{n_classes}_{mask_percentage}"
 
 variation = "unet"
 
 if variation == "vanilla":
     model = Autoencoder_conv()
-    figure_path = Path(figure_folder / f"vanilla_{identifier}.png")
+    figure_path = Path(losses_folder / f"vanilla_{identifier}.png")
     model_weights_name = f"vanilla_{identifier}.pth"
     
 elif variation == "unet":
     model = Autoencoder_unet()
-    figure_path = Path(figure_folder / f"unet_{identifier}.png")
+    figure_path = Path(losses_folder / f"unet_{identifier}.png")
     model_weights_name = f"unet_{identifier}.pth"
     
 else:
@@ -121,14 +127,14 @@ print(f"Training finished in {time.time() - start_time} seconds")
     
 
 print("Saving model")
-if not model_folder.exists():
-    model_folder.mkdir(parents=True, exist_ok=True)
-th.save(model.state_dict(), model_folder / model_weights_name)
+if not weights_folder.exists():
+    weights_folder.mkdir(parents=True, exist_ok=True)
+th.save(model.state_dict(), weights_folder / model_weights_name)
 print("Model saved")
 
 print("Saving training loss plot")
-if not figure_folder.exists():
-    figure_folder.mkdir(parents=True, exist_ok=True)
+if not losses_folder.exists():
+    losses_folder.mkdir(parents=True, exist_ok=True)
 plt.plot(train_losses, label="Train")
 plt.plot(test_losses, label="Test")
 plt.legend()
