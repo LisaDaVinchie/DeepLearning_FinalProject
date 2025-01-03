5abfeeae3b210d74d662d4092d914245b081e177
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-03 09:54:37 +0100

    Modified training sbatch file

--- src/train.sh
+++ src/train.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=train_model
-#SBATCH --output=logs.%j.out
-#SBATCH --error=logs.%j.err
-#SBATCH --time=0-00:30
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+#SBATCH --time=00:00:10
+#SBATCH --nodes=1
+#SBATCH --ntasks=18
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
 
 set -e
 
@@ -12,9 +14,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
     source ../../DL_env/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 fi
 
-module load cuda/ || { echo "Failed to load cuda module"; exit 1; }
 
 make train || { echo "Training failed"; exit 1; }
 
 echo "Training completed successfully"
-module unload cuda || { echo "Failed to unload cuda module"; exit 1; }
