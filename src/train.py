33117cb4ff64cbaa11ec2bbe15d9e9becefbdee6
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-16 17:36:28 +0100

    Better parameter import, no result overwriting, more results info

--- src/train.py
+++ src/train.py
@@ -7,10 +7,12 @@ import time
 import json
 import argparse
 
-from utils.parameter_selection import filter_params, typecast_bool
+from utils.parameter_selection import filter_params
+from utils.load_config import load_params
 from utils.metrics import calculate_psnr, calculate_ssim
 from utils.get_workers_number import get_available_cpus
 from utils.to_black_and_white import dataset_to_black_and_white
+from utils.increment_filepath import increment_filepath
 from models.ImageDataset import CustomImageDataset
 from models.transformer import TransformerInpainting
 import models.autoencoder as autoencoder
@@ -46,28 +48,33 @@ if not weights_path.parent.exists():
 if not results_path.parent.exists():
     results_path.parent.mkdir(parents=True, exist_ok=True)
 
+# Loading parameters
 params_config_path = args.params
+n_train = None
+n_test = None
+epochs = None
+batch_size = None
+learning_rate = None
+scheduler = None
+rgb = None
+initialize = None
+sched_step = None
+sched_gamma = None
+model_name = None
+
+dataset_params = load_params(params_config_path, "dataset_params")
+train_params = load_params(params_config_path, "train_params")
+
+locals().update(dataset_params)
+locals().update(train_params)
+
+
+n_channels: int = 1 + 2 * int(rgb)
+print("rgb", rgb, flush=True)
+
 with open(params_config_path, "r") as f:
     config = json.load(f)
-
-n_train = int(config["n_train"])
-n_test = int(config["n_test"])
-batch_size = int(config["batch_size"])
-learning_rate = float(config["learning_rate"])
-epochs = int(config["epochs"])
-scheduler = typecast_bool(config["scheduler"])
-initialize = typecast_bool(config["initialize"])
-sched_step = int(config["sched_step"])
-sched_gamma = float(config["sched_gamma"])
-image_size = int(config["image_width"])
-rgb = typecast_bool(config["rgb"])
-
-if rgb:
-    n_channels: int = 3
-else:
-    n_channels: int = 1
-
-model_name = config.get("model_name")
+    
 model_params = config.get("model_configs", {}).get(model_name, {})
 
 # Mapping model names to classes
@@ -124,12 +131,13 @@ else:
     scheduler = None
     
 def initialize_weights(module):
-    if isinstance(module, th.nn.Conv2d) or isinstance(module, th.nn.Linear):
-        th.nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+    if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+        nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
         if module.bias is not None:
-            th.nn.init.zeros_(module.bias)
+            nn.init.zeros_(module.bias)
 if initialize:
     model.apply(initialize_weights)
+    
 criterion = nn.MSELoss()
 
 if device == th.device("cpu"):
@@ -148,7 +156,7 @@ test_ssim = []
 
 learning_rates = []
 
-def get_metrics(model: th.nn.Module, device: th.device, criterion: th.nn.Module, image: th.tensor, mask: th.tensor) -> tuple:
+def get_metrics(model: nn.Module, device: th.device, criterion: nn.Module, image: th.tensor, mask: th.tensor) -> tuple:
     image.to(device)
     mask.to(device)
     output = model(image, mask)
@@ -201,6 +209,7 @@ for epoch in range(epochs):
         
 print(f"Training finished in {time.time() - start_time} seconds", flush=True)
 
+
 metrics_data = {
     "train_loss": train_losses,
     "test_loss": test_losses,
@@ -209,22 +218,21 @@ metrics_data = {
     "train_ssim": train_ssim,
     "test_psnr": test_psnr,
     "test_ssim": test_ssim,
-    "model_name": model_name,
-    "model_params": model_params
 }
 
-# Check if results_path already exists and modify the path if necessary
-if results_path.exists():
-    base = results_path.stem
-    suffix = results_path.suffix
-    parent = results_path.parent
-    counter = 1
-    while results_path.exists():
-        results_path = parent / f"{base}_{counter}{suffix}"
-        counter += 1
-
+results_path = increment_filepath(results_path)
 with open(results_path, "w") as f:
     json.dump(metrics_data, f)
-    
+
+extra_info = f"Model: {model_name}\nModel parameters: {model_params}\nDataset parameters: {dataset_params}\nTraining Time: {time.time() - start_time} seconds\n\n\n"
+
+with open(results_path.with_suffix(".txt"), "w") as f:
+    f.write(extra_info)
+
+weights_path = increment_filepath(weights_path)
 th.save(model.state_dict(), weights_path)
+
+extra_info = f"Model: {model_name}\nModel parameters: {model_params}\nDataset parameters: {dataset_params}\nTraining Time: {time.time() - start_time} seconds\n\n\n"
+with open(weights_path.with_suffix(".txt"), "w") as f:
+    f.write(extra_info)
 print("Model saved", flush=True)
