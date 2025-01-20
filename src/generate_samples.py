c8e4718138477cc7d71cae2fd4e4b2aae5fb9ec2
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-20 16:08:15 +0100

    Improved sample generation

--- src/generate_samples.py
+++ src/generate_samples.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import torch as th
+from torch.utils.data import DataLoader
 from pathlib import Path
 
 def reconstruct_image(image, output, mask):
@@ -7,36 +8,38 @@ def reconstruct_image(image, output, mask):
     reconstructed_image[mask] = output[mask]
     return reconstructed_image
 
-def sample_generation(model: th.nn.Module, dataset: dict, n_samples: int, figs_folder: Path):
-    total_images = len(dataset["images"])
+def sample_generation(model: th.nn.Module, dataloader: DataLoader, n_samples: int, figs_folder: Path):
+    dataset = dataloader.dataset
+    total_images = len(dataloader)
     
     sample_idx = th.randint(0, total_images, (n_samples,))
     
-    for idx in sample_idx:
-        image = dataset["images"][idx].unsqueeze(0)
-        mask = dataset["masks"][idx].unsqueeze(0)
+    with th.no_grad():
+        for idx in sample_idx:
+            image = dataset[idx][0].unsqueeze(0)
+            mask = dataset[idx][1].unsqueeze(0)
 
-        print(image.shape, mask.shape)
+            print(image.shape, mask.shape)
 
-        model.eval()
-        with th.no_grad():
-            output = model(image, mask)
-            
-        rec_image = reconstruct_image(image, output, mask)
+            model.eval()
+            with th.no_grad():
+                output = model(image, mask)
+                
+            rec_image = reconstruct_image(image, output, mask)
 
-        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+            fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
-        axs[0].imshow(image.squeeze().permute(1, 2, 0))
-        axs[0].set_title("Original")
-        axs[0].axis("off")
+            axs[0].imshow(image.squeeze().permute(1, 2, 0))
+            axs[0].set_title("Original")
+            axs[0].axis("off")
 
-        axs[1].imshow((image * mask).squeeze().permute(1, 2, 0))
-        axs[1].set_title("Masked")
-        axs[1].axis("off")
+            axs[1].imshow((image * mask).squeeze().permute(1, 2, 0))
+            axs[1].set_title("Masked")
+            axs[1].axis("off")
 
-        axs[2].imshow(rec_image.squeeze().permute(1, 2, 0))
-        axs[2].set_title("Inpainted")
-        axs[2].axis("off")
+            axs[2].imshow(rec_image.squeeze().permute(1, 2, 0))
+            axs[2].set_title("Inpainted")
+            axs[2].axis("off")
 
-        fig.savefig(figs_folder / f"sample_{idx}.png")
-        plt.close(fig)
+            fig.savefig(figs_folder / f"sample_{idx}.png")
+            plt.close(fig)
