bba9dfe14ec5d48364d3937facafd0cb73f96907
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-20 10:52:02 +0100

    Improved image masking

--- src/models/mask_image.py
+++ src/models/mask_image.py
@@ -3,6 +3,8 @@ import torch as th
 def mask_image(image: th.tensor, mask: th.tensor, placeholder = None) -> th.tensor:
     
     if placeholder is None:
-        placeholder = th.mean(image).detach()
+        placeholder = th.mean(image, dim=(1, 2), keepdim=True).detach()
+        
+    print(placeholder)
         
     return th.where(mask, placeholder, image)
