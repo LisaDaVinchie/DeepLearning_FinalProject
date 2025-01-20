366af9586997806178cc145bf168b250822aaee1
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-20 11:00:55 +0100

    Small bug fix

--- src/models/mask_image.py
+++ src/models/mask_image.py
@@ -5,6 +5,4 @@ def mask_image(image: th.tensor, mask: th.tensor, placeholder = None) -> th.tens
     if placeholder is None:
         placeholder = th.mean(image, dim=(1, 2), keepdim=True).detach()
         
-    print(placeholder)
-        
     return th.where(mask, placeholder, image)
