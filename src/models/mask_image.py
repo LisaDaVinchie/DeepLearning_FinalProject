df9c0a7781fd6cf6950f122a57bed97e58d806cf
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-21 12:50:07 +0100

    Bug fix

--- src/models/mask_image.py
+++ src/models/mask_image.py
@@ -3,11 +3,11 @@ import torch as th
 def mask_image(image: th.tensor, mask: th.tensor, placeholder = None) -> th.tensor:
     
     if placeholder is None:
-        placeholder = th.mean(~image, dim=(2, 3), keepdim=True).detach()
+        placeholder = th.mean(image, dim=(2, 3), keepdim=True).detach()
     # print(f"Image shape: {image.shape}, mask shape: {mask.shape}, placeholder shape: {placeholder.shape}")
     
     try:
-        masked_image = th.where(mask, image, placeholder) 
+        masked_image = th.where(~mask, image, placeholder) 
     except Exception as e:
         print(f"Error: {e}")
         print(f"Image shape: {image.shape}, mask shape: {mask.shape}, placeholder shape: {placeholder.shape}")
