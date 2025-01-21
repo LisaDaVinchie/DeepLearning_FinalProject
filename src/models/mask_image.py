b8e492d4a548dbf0dabae94728b42c1461a726b1
LisaDaVinchie <lisadavinchie@gmail.com>
2025-01-21 12:32:12 +0100

    Big error fix

--- src/models/mask_image.py
+++ src/models/mask_image.py
@@ -3,7 +3,7 @@ import torch as th
 def mask_image(image: th.tensor, mask: th.tensor, placeholder = None) -> th.tensor:
     
     if placeholder is None:
-        placeholder = th.mean(image, dim=(2, 3), keepdim=True).detach()
+        placeholder = th.mean(~image, dim=(2, 3), keepdim=True).detach()
     # print(f"Image shape: {image.shape}, mask shape: {mask.shape}, placeholder shape: {placeholder.shape}")
     
     try:
